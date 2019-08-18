da7167b50491d0a8ec584168adec0f4beeb5906c
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-18 04:00:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash = { best_rapper: Vanilla Ice, worst_rapper: 
 end
 
 def pioneer
